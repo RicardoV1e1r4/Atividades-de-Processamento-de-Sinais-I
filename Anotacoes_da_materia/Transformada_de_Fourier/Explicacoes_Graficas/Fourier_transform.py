c2adfff999eec28c0d817940b4598201095f8688
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-06-06 16:53:46 -0300

    Ajustes na função
    Foi alterado a função atualizar para conseguir alterar o gráfico discreto, mas não funcionou.
    E foi alterado um pouco organização.

--- Anotacoes_da_materia/Transformada_de_Fourier/Explicacoes_Graficas/Fourier_transform.py
+++ Anotacoes_da_materia/Transformada_de_Fourier/Explicacoes_Graficas/Fourier_transform.py
@@ -7,76 +7,95 @@ Created on Tue Jun  2 17:47:08 2026
 
 import matplotlib.pyplot as plt
 import numpy as np
-#from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider
 
 # Valores iniciais do Sliders
-
-
-
-# # Taxa de amostragem
-# def taxa_amostragem():
-#     return Fs
-    
-
-
-# # Frequência angular
-# def frequencia_angular():
-#     return w
-
-
-# Vetor tempo
+# -Vetor tempo
 fs = 1
 Ts = 1/fs
-t_final = 40
+t_final = 60
 t = np.arange(0, t_final, Ts)
 
-# Sinal x[n]
-x_n = np.cos(t)
+omega = 0.1
 
-# Sinal variável
-h_n = np.sin(t)
+
+# Taxa de amostragem
+def geracao_sinal(w, const=False):
+    if const==True:
+        # Sinal x[n]
+        y_n = np.cos(t)
+    else:
+        # Sinal variável h[h]
+        y_n = np.sin(w*t)
+    
+    return y_n
 
 # Criação dos gráficos
-grafico, (linha1, linha2) = plt.subplots(2, 1, figsize=(7,7))
+figure, (eix1, eix2) = plt.subplots(2, 1, figsize=(8,7))
 # Espaço extra para os botões deslizantes
-plt.subplots_adjust(left=0.2, bottom=0.40, hspace=0.6)
+plt.subplots_adjust(left=0.12, bottom=0.15, hspace=0.5)
 
 # configuração do gráfico do sinal x[n] original
-linha1.stem(t, x_n)
-linha1.set_xlabel('Time [s]', size=10)
-linha1.set_ylabel('Amplitude', size=10)
-linha1.set_title('Sinal x[n]')
-linha1.grid(True)
+eix1.stem(t, geracao_sinal(omega, True))
+eix1.set_xlabel('Time [s]', size=10)
+eix1.set_ylabel('Amplitude', size=10)
+eix1.set_title('Sinal x[n]')
+eix1.grid(True)
 
 # Configuração do gráfico do 
-linha2.stem(t, h_n)
-#grafc2, = linha2.stem(t, np.cos(frequencia_angular*t), color='r')
-linha2.set_xlabel('Time [s]', size=10)
-linha2.set_ylabel('Amplitude', size=10)
-linha2.set_title('Sinal variável')
-linha2.grid(True)
+# eix2.stem(t, h_n)
+stem2 = eix2.stem(t, geracao_sinal(omega, False),
+                  linefmt='C0-',
+                  markerfmt='C0o',
+                  basefmt='k-')
+
+
+eix2.set_xlabel('Time [s]', size=10)
+eix2.set_ylabel('Amplitude', size=10)
+eix2.set_title('Sinal variável')
+eix2.grid(True)
 
 ###_____BOTÕES DESLIZANTES_____###
-#Deslizante 1 - raio da esfera A
-# f_sample = plt.axes([0.2, 0.25, 0.65, 0.03])
-# slider_raioa = Slider(f_sample, 'Raio A', 0.1, 40, valinit=a)
+# Deslizante 1 - raio da esfera A
+# fsample = plt.axes([0.2, 0.25, 0.65, 0.03])
+# slider_sample = Slider(fsample, 'Amostragem', 0.01, 5, valinit=fs)
 
-# #Deslizante 2 - raio da esfera B
-# omega = plt.axes([0.2, 0.18, 0.65, 0.03])
-# slider_raiob = Slider(omega, 'Raio B', 0.1, 40, valinit=b)
+# #Deslizante 2 - Frequência angular
+freq_ang = figure.add_axes([0.2, 0.25, 0.65, 0.03])
+
+slider_omega = Slider(
+    ax=freq_ang,
+    label='Frequêncai angular',
+    valmin=0.1,
+    valmax=30,
+    valinit=omega,)
+
+###___Função que vai atualizar o gráfico___###
+def atualizar(val):
+    global stem2
+    omega = slider_omega.val
+    
+    # Remover o gráfico anterior
+    stem2.makerline.remove()
+    
+    try:
+        stem2.stemlines.remove()
+    except:
+        for linha in stem2.stemlines:
+            linha.remove()
+            
+    stem2.baseline.remove()
+    
+    # Cria o novo gráfico stem
+    stem2 = eix2.stem(t, geracao_sinal(omega, False))
+    
+    # raioa = slider_raioa.val
+    # eix1.set_ydata(PreencheDensidade(raioa, raiob))
+    figure.canvas.draw_idle()
 
 
-# ###___Função que vai atualizar o gráfico___###
-# def atualizar(val):
-#     raioa = slider_raioa.val
-#     raiob = slider_raiob.val
-#     linha1.set_ydata(PreencheDensidade(raioa, raiob))
-#     linha2.set_ydata(PreencheDensidade(raioa, raiob, True))
-#     figura.canvas.draw_idle()
-
-
-# # Conecta os slides à função
+# Conecta os slides à função
 # slider_raioa.on_changed(atualizar)
-# slider_raiob.on_changed(atualizar)
+slider_omega.on_changed(atualizar)
 
 plt.show()
