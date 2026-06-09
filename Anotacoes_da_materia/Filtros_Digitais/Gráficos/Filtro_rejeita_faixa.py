66b03135a093077aa804709a5acca8d73bc3cddc
Ricardo <131534712+RicardoV1e1r4@users.noreply.github.com>
2026-06-09 16:07:22 -0300

    Update Filtro_rejeita_faixa.py

--- Anotacoes_da_materia/Filtros_Digitais/Gráficos/Filtro_rejeita_faixa.py
+++ Anotacoes_da_materia/Filtros_Digitais/Gráficos/Filtro_rejeita_faixa.py
@@ -11,13 +11,14 @@ from scipy.signal import freqz
 
 # parâmetros dos polos
 r = 0.8
-wp1 = 0.2*2*np.pi
-wp2 = 0.4*2*np.pi
 
-mb1 = -2*r*np.cos(wp1)
+w1 = 0.25
+w2 = 0.35
+
+mb1 = -2*r*np.cos(w1)
 mb2 = r**2
 
-ma1 = -2*r*np.cos(wp2)
+ma1 = -2*r*np.cos(w2)
 ma2 = r**2
 
 # coeficientes do passa baixa
@@ -34,17 +35,17 @@ w1, H1 = freqz(num1, den1, 2048)
 # resposta em frequência passa-alta
 w2, H2 = freqz(num2, den2, 2048)
 
-figure = plt.subplots(figsize=(7,6))
+figure = plt.subplots(figsize=(8,6.5))
 
-eix1 = plt.subplot(2, 1, 1)
+eix1 = plt.subplot(2, 2, 1)
 eix1.plot(w1/(2*np.pi), np.abs(H1))
 eix1.plot(w2/(2*np.pi), np.abs(H2))
-eix1.set_xlabel(r'$\omega/\pi$')
+eix1.set_xlabel(r'$\omega$ (rad/amostras)')
 eix1.set_ylabel(r'$|H(e^{j\omega})|$')
-eix1.set_title('Filtro Passa-Faixa de 2ª Ordem')
+eix1.set_title('Filtro Rejeita-Faixa de 2ª Ordem')
 eix1.grid(True)
 
-eix2 = plt.subplot(2, 1, 2, projection='polar')
+eix2 = plt.subplot(2, 2, 3, projection='polar')
 eix2.plot(w1/np.pi, np.abs(H1))
 eix2.plot(w2/np.pi, np.abs(H2))
 eix2.set_xlabel(r'$\omega/\pi$')
